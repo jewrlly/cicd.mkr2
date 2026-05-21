9a18c7f9998596c29dd2029c4e283430a1703c02
Polina <polinaonipcuk@gmail.com>
2026-05-21 13:00:39 +0300

    feat: add category_detail view

--- recipe/views.py
+++ recipe/views.py
@@ -9,4 +9,6 @@ def main(request):
     random_recipes = random.sample(recipes, min(10, len(recipes)))
     return render(request, 'main.html', {'recipes': random_recipes})
 
-
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'category_detail.html', {'category': category})
